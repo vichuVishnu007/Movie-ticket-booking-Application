4132944d1a1807cf0f240fcb6fe24a925838acde
vichuVishnu007 <84537440+vichuVishnu007@users.noreply.github.com>
2023-06-12 20:32:05 +0530

    Delete Backend/MovieManagement directory

--- Backend/MovieManagement/src/main/java/com/example/demo/exceptions/DuplicateMovieIdException.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.exceptions;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code= HttpStatus.CONFLICT, reason="Movie ID already exists, handled by custom exception")
-public class DuplicateMovieIdException extends Exception{
-
-}
